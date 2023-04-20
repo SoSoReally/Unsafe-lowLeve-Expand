517a57c86432dcc8ffa5870f9e31da6e85aecbcc
SoSoReally <534605060@qq.com>
2023-04-20 13:31:04 +0800

    Update Unsafe.cs

--- Unsafe/Unsafe.cs
+++ Unsafe/Unsafe.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices.WindowsRuntime;
 using Unity.Collections;
 using Unity.Collections.LowLevel.Unsafe;
 
@@ -15,32 +16,53 @@ public static partial class UnsafeCode
     {
         return ref Unsafe.As<byte, T>(ref Unsafe.AsRef(in span.GetPinnableReference()));
     }
+    public unsafe static ref readonly T AsReadOnly<T>(this Span<byte> span) where T : unmanaged
+    {
+        return ref Unsafe.As<byte, T>(ref Unsafe.AsRef(in span.GetPinnableReference()));
+    }
+
+
+    public unsafe static NativeArray<T> ArrayOnlyOneElement<T>(in T value ) where T : unmanaged
+    {
+        var na = new NativeArray<T>(1,Allocator.Temp);
+        na[0] = value;
+        return na;
+    }
 
 
     #region Get IntPtr
-    public unsafe static IntPtr GetUnSafeIntPtr<T>(this NativeSlice<T> values) where T : unmanaged
+    public unsafe static IntPtr GetBufferUnSafeIntPtr<T>(this ref NativeSlice<T> values) where T : unmanaged
     {
 
         return new IntPtr(values.GetUnsafePtr());
     }
 
-    public unsafe static IntPtr GetUnSafeIntPtr<T>(this NativeArray<T> values) where T : unmanaged
+    public unsafe static IntPtr GetBufferUnSafeIntPtr<T>(this ref NativeArray<T> values) where T : unmanaged
     {
         return new IntPtr(values.GetUnsafePtr());
     }
 
-    public unsafe static IntPtr GetIntPtr<T>(this Span<T> values) where T : unmanaged
+    public unsafe static IntPtr GetIntPtr<T>(this ref Span<T> values) where T : unmanaged
     {
         return new IntPtr(UnsafeUtility.AddressOf<T>(ref values.GetPinnableReference()));
+    }
+
+    public unsafe static IntPtr AsIntPtr<T>(this ref T values) where T : unmanaged
+    {
+        return new IntPtr(UnsafeUtility.AddressOf(ref values));
     }
     #endregion
 
     #region As Span and ReadOnlySpan
-    public unsafe static Span<T> AsSpan<T>(this NativeSlice<T> values) where T : unmanaged
+    public unsafe static ref T As<T>(this IntPtr intPtr) where T : unmanaged
+    {
+        return ref Unsafe.AsRef<T>(intPtr.ToPointer());
+    }
+    public unsafe static Span<T> AsSpan<T>(this ref NativeSlice<T> values) where T : unmanaged
     {
         return new Span<T>(values.GetUnsafePtr(), values.Length);
     }
-    public unsafe static ReadOnlySpan<T> AsReadOnlySpan<T>(ref this NativeSlice<T> values) where T : unmanaged
+    public unsafe static ReadOnlySpan<T> AsReadOnlySpan<T>( this in NativeSlice<T> values) where T : unmanaged
     {
         return new ReadOnlySpan<T>(values.GetUnsafePtr(), values.Length);
     }
@@ -52,9 +74,13 @@ public static partial class UnsafeCode
     {
         return new ReadOnlySpan<T>(ptr.ToPointer(), length);
     }
-    public unsafe static Span<T> AsSpan<T>(this NativeList<T> nativeList) where T : unmanaged
+    public unsafe static Span<T> AsSpan<T>(this ref NativeList<T> nativeList) where T : unmanaged
     {
         return new Span<T>(nativeList.GetUnsafeList()->Ptr, nativeList.GetUnsafeList()->Length);
+    }
+    public unsafe static ReadOnlySpan<T> AsReadOnlySpan<T>(this in NativeList<T> nativeList) where T : unmanaged
+    {
+        return new ReadOnlySpan<T>(nativeList.GetUnsafeList()->Ptr, nativeList.GetUnsafeList()->Length);
     }
     #endregion
 
@@ -70,7 +96,11 @@ public static partial class UnsafeCode
     /// <returns></returns>
     public unsafe static NativeArray<T> AsNativeArray<T>(this Span<T> ns) where T : unmanaged
     {
-        return CollectionHelper.ConvertExistingDataToNativeArray<T>(UnsafeUtility.AddressOf<T>(ref ns.GetPinnableReference()), ns.Length, Allocator.None, true);
+        fixed(void* b = &ns[0])
+        {
+            return CollectionHelper.ConvertExistingDataToNativeArray<T>(b, ns.Length, Allocator.None, true);
+        }
+        
         //        var result = NativeArrayUnsafeUtility.ConvertExistingDataToNativeArray<T>(UnsafeUtility.AddressOf(ref ns[0]), ns.Length, Allocator.None);
         //#if ENABLE_UNITY_COLLECTIONS_CHECKS
         //        var safety = AtomicSafetyHandle.GetTempMemoryHandle();
@@ -111,16 +141,16 @@ public static partial class UnsafeCode
         return new Unity.Collections.LowLevel.Unsafe.UnsafeList<T>((T*)Unsafe.AsPointer(ref ns[0]), ns.Length);
     }
 
-    public unsafe static Span<T> AsSpan<T>(this UnsafeList<T> ts) where T : unmanaged
+    public unsafe static Span<T> AsSpan<T>(this ref UnsafeList<T> ts) where T : unmanaged
         {
             return new Span<T>(ts.Ptr, ts.Length);
         }
-    public unsafe static ReadOnlySpan<T> AsReadOnlySpan<T>(this UnsafeList<T> ts) where T : unmanaged
+    public unsafe static ReadOnlySpan<T> AsReadOnlySpan<T>(this ref UnsafeList<T> ts) where T : unmanaged
     {
         return new ReadOnlySpan<T>(ts.Ptr, ts.Length);
     }
 
-    public unsafe static ref T UnsafeElementAt<T>(this UnsafeList<T> ts,int index) where T : unmanaged
+    public unsafe static ref T UnsafeElementAt<T>(this ref UnsafeList<T> ts,int index) where T : unmanaged
     {
         return ref ts.Ptr[index];
     }
@@ -128,11 +158,16 @@ public static partial class UnsafeCode
 
     #region NativeContainer to NativeContainer
 
-    public unsafe static NativeArray<T> ToNativeArray<T>(this NativeSlice<T> ns ,Allocator allocator)where T : unmanaged
+    public unsafe static NativeArray<T> ToNativeArray<T>(this ref NativeSlice<T> ns ,Allocator allocator)where T : unmanaged
     {
         NativeArray<T> result = new NativeArray<T>(ns.Length,allocator);
         ns.CopyTo(result);
         return result;
+    }
+
+    public unsafe static void AddRange<T>(this ref UnsafeList<T> us, in NativeArray<T> values) where T : unmanaged
+    {
+        us.AddRangeNoResize(values.GetUnsafePtr(), values.Length);
     }
 
     #endregion
@@ -141,7 +176,5 @@ public static partial class UnsafeCode
     {
         values.GetUnsafeList()->AddRange(UnsafeUtility.AddressOf<T>(ref values1.GetPinnableReference()), values1.Length);
     }
-
-
 
 }
