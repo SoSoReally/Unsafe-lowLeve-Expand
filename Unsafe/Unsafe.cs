b3b71d2da4072e56b47434fb262b10b860753690
SoSoReally <534605060@qq.com>
2024-05-23 16:47:08 +0800

    fix Unsafelist asReadonlySpan

--- Unsafe/Unsafe.cs
+++ Unsafe/Unsafe.cs
@@ -87,8 +87,8 @@ public static partial class UnsafeCode
     #region NativeContainer As From Span and ReadOnlySpan
 
     /// <summary>
-    /// 需要保证原子操作安全
-    /// span 总是 none，copy 使用 <see cref="ToNativeArray{T}(Span{T}, Allocator)"/>
+    /// 锟斤拷要锟斤拷证原锟接诧拷锟斤拷锟斤拷全
+    /// span 锟斤拷锟斤拷 none锟斤拷copy 使锟斤拷 <see cref="ToNativeArray{T}(Span{T}, Allocator)"/>
     /// </summary>
     /// <typeparam name="T"></typeparam>
     /// <param name="ns"></param>
@@ -145,7 +145,7 @@ public static partial class UnsafeCode
         {
             return new Span<T>(ts.Ptr, ts.Length);
         }
-    public unsafe static ReadOnlySpan<T> AsReadOnlySpan<T>(this ref UnsafeList<T> ts) where T : unmanaged
+    public unsafe static ReadOnlySpan<T> AsReadOnlySpan<T>(this in UnsafeList<T> ts) where T : unmanaged
     {
         return new ReadOnlySpan<T>(ts.Ptr, ts.Length);
     }
